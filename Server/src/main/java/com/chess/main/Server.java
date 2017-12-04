fdcfe62ddaaf6f716000238dca14c9779111a907
hrynchyshynHub <vaniahrynchyshyn@gmail.com>
2017-12-04 15:19:16 +0200

    separated Server and Main class

--- Server/src/main/java/com/chess/main/Server.java
+++ Server/src/main/java/com/chess/main/Server.java
@@ -1,59 +1,47 @@
 package com.chess.main;
 
-import com.chess.model.Board;
-import com.chess.model.pieces.Bishop;
-import com.chess.model.pieces.Piece;
-import com.chess.util.Color;
 import org.apache.log4j.Logger;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Created by ivan.hrynchyshyn on 15.11.2017.
  */
-public class Server extends Thread{
-     Socket socket;
-     int connectionCount = 0;
-     private static  final Logger logger = Logger.getLogger(Server.class);
+public class Server implements Runnable{
+     private static ServerSocket socket;
+     private Socket fistPlayerSocket;
+     private Socket secondPlayerSocket;
+     private ObjectInputStream ois;
+     private ObjectOutputStream oos;
+     private AtomicInteger connectionCount = new AtomicInteger();
+     private static final Logger logger = Logger.getLogger(Server.class);
 
-    public Server(Socket socket, int connectionCount){
-        this.socket = socket;
-        this.connectionCount = connectionCount ;
-        setDaemon(true);
-        setPriority(NORM_PRIORITY);
-        start();
+    public Server(){
+        try {
+            socket = new ServerSocket(5556, 0, InetAddress.getByName("localhost"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void run() {
-        try (InputStream is = socket.getInputStream();
-             OutputStream os = socket.getOutputStream()) {
-            byte buf[] = new byte[64 * 1024];
-            int r = is.read(buf);
-            String data = new String(buf, 0, r);
-            data = "" + connectionCount + ": " + "\n" + data;
-            os.write(data.getBytes());
-        } catch (Exception e) {
-            logger.error(e);
-        }
-    }
-
-    public static void main(String[] args) {
-        try(ServerSocket serverSocket = new ServerSocket(5556, 0 , InetAddress.getByName("localhost"));){
-            int i = 0;
-            while (!Thread.interrupted()){
-                new Server(serverSocket.accept(), i);
-                i++;
+        while (true){
+            try{
+                fistPlayerSocket = socket.accept();
+                secondPlayerSocket = socket.accept();
+//                ois = new ObjectInputStream(.getInputStream());
+//                oos = new ObjectOutputStream(clientSocket.getOutputStream());
+                connectionCount.incrementAndGet();
+                System.out.println("Connection: " + connectionCount);
+                System.out.println(fistPlayerSocket.getInputStream().read());
+            }catch (IOException e){
+                   e.printStackTrace();
             }
-        } catch (IOException e) {
-           logger.error(e);
+
         }
-
     }
-
 
 }
